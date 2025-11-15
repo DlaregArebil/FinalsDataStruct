52edc860eed2ce4c2ee6cd330f78c70bb2f35e5c
DlaregArebil <geraldalenlibera11@gmail.com>
2025-11-15 13:41:30 +0800

    Update Mainmain.java

--- Mainmain.java
+++ Mainmain.java
@@ -1,160 +1,166 @@
 package FinalProject;
+import java.io.*;
 import java.lang.*;
 import java.util.*;
 import java.util.regex.*;
 
-public class Mainmain {
+public class ClientClass {
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 
-    String art = """
-                                                                                     
-                                           *****                                                    
-                                        ++##%%%%###                                                 
-                                      +##%%@@@@@@%%#*                                               
-                                     **%%@@@@@@@@@@@%#                                              
-                                     #%@%#*%###%#%%@@@%                                             
-                                     #%#=+*##**#%#*#@@                                              
-                                     **==+#%*++#%#++%%                                              
-                                     *+--===+===++==**                                              
-                                     ++-=+++*##*+===+                                               
-                                       -=+++*##*++++*                                               
-                                       -=++*****+++                                                 
-                                     -==+******+++                                                   
-                                  -----+**####**+                                                    
-                             ==-==+=-==-=******++=-                                                  
-                           ====-====-*##*++++===*===                                                 
-                         =----------=====++=====+=++---                                              
-                        +=----++--=+=---=+*+====*-==:---                                             
-                       *++=--=*-:-:-==----=+*+==*=-----=                                             
-                      +++====+*=:---=----=+=---=+=-:-===                                             
-                     ====---==*-:-====--==-::::=------=-+                                            
-                    =====--=+=++++++------=-:::=----==+--                                            
-                    +=-==++==+++==--:-===------+=:-==+==-                                            
-                   -====++++=--=-:--::-+=======---=--+**=+                                           
-                   ===+++**+=-==::--:-=++-----++:--:++=--=                                           
-                  --+=----=++==+----=++++++++===----+++++=                                           
-                  =+==-----=+**++++++====+++=--=+-:=##*+===                                          
-                  ===-------+*+--=+==----=+==-:-====****+==-                                         
-                    --==--==+#=::---=----=-----:---=  #***+=--                                       
-                    +++++++-=#-::-=-----=:---======-    #***+==::                                    
-                    +++++==+**--=+=-::-==-::--==++=-      #**+++*%                                   
-                    +++++==+++=++=::::::+=====+=-==::       *+==--=                                  
-                     +++++==+#*++::::::--::::-:::::-=       +++=---                                  
-                     ++++++=+++++-:::---------::::-=+       *+++====                                 
-                      ++++===++=--==+++=--:--=-::--+==       *++====                                 
-                      *++++==++=---++-::----:::-++=::--       +++*++                                 
-                       *++++==+====::::::-+---==+=:::::-      =+**                                   
-                        +++++=+===-::::::=+=--=+=--::-=-                                            
-                        *++++==+*+-::::=+==++=----:::---=                                           
-                         +++++==*+=-:--==+++=------:-=-:-                                           
-                         -=+++==+=::----+===-::---=--::-=-                                          
-                """;
-
-        System.out.println(art);
-
-    // public static boolean nameValidation(String clientFName){
-    //     Pattern patternName = Pattern.compile("^[A-Z][a-z]+(?: [A-Z][a-z]+)+$"); //rule
-    //     Matcher matcherName = patternName.matcher(clientFName); //inspect/check
-    //     boolean isMatchFoundName;
-    //     isMatchFoundName = matcherName.matches(); //validate
-
-    //     if (isMatchFoundName){
-    //             return true;
-    //     } else {
-    //             return false;
-    //     }
-    // }
-
-    // public static boolean addressValidation(String clientAdd){
-    //     Pattern patternAdd = Pattern.compile("^\\d+\\s[A-Za-z0-9\\s]+,\\s[A-Za-z\\s]+,\\s[A-Za-z0-9\\s]+$"); //street num, city name,  
-    //     Matcher matcherAdd = patternAdd.matcher(clientAdd); //inspect/check
-    //     boolean isMatchFoundAdd;
-    //     isMatchFoundAdd = matcherAdd.matches(); //validate
-
-    //     if (isMatchFoundAdd){
-    //             return true;
-    //     } else {
-    //             return false;
-    //     }
-    // }
-
-    public static boolean continfoValidation(String clientCInfo){
-        Pattern patternCI = Pattern.compile("^0\\d{10}$"); //rule
-        Matcher matcherCI = patternCI.matcher(clientCInfo); //inspect/check
+    public static boolean continfoValidation(String clientCInfo) {
+        Pattern patternCI = Pattern.compile("^0\\d{10}$"); // rule
+        Matcher matcherCI = patternCI.matcher(clientCInfo); // inspect/check
         boolean isMatchFoundCI;
-        isMatchFoundCI = matcherCI.matches(); //validate
+        isMatchFoundCI = matcherCI.matches(); // validate
 
-        if (isMatchFoundCI){
-                return true;
+        if (isMatchFoundCI) {
+            return true;
         } else {
-                return false;
+            return false;
         }
     }
 
-    public static boolean emailValidation(String clientEAdd){
-        Pattern patternEmail = Pattern.compile("^[\\w.-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"); //rule
-        Matcher matcherEmail = patternEmail.matcher(clientEAdd); //inspect/check
+    public static boolean emailValidation(String clientEAdd) {
+        Pattern patternEmail = Pattern.compile("^[\\w.-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"); // rule
+        Matcher matcherEmail = patternEmail.matcher(clientEAdd); // inspect/check
         boolean isMatchFoundEmail;
-        isMatchFoundEmail = matcherEmail.matches(); //validate
+        isMatchFoundEmail = matcherEmail.matches(); // validate
 
-        if (isMatchFoundEmail){
-                return true;
+        if (isMatchFoundEmail) {
+            return true;
         } else {
-                return false;
+            return false;
         }
     }
 
+    public static int ClientID(ArrayList<String> CInfo) {
+
+        int sum = 0;
+
+        for (int i = 0; i < CInfo.size(); i++) {
+            String phone = CInfo.get(i);
+
+            int[] digits = new int[phone.length()];
+            for (int j = 0; j < phone.length(); j++) {
+                digits[j] = Character.getNumericValue(phone.charAt(j));
+            }
+
+            // take not sa mag dodocumentary BUBBLE SORTING PO ITO HEHEHE
+            // converting your phone number into ascending order
+            for (int j = 0; j < digits.length - 1; j++) {
+                for (int k = 0; k < digits.length - 1 - j; k++) {
+                    if (digits[k] > digits[k + 1]) {
+                        int temp = digits[k];
+                        digits[k] = digits[k + 1];
+                        digits[k + 1] = temp;
+                    }
+                }
+            }
+
+            for (int j = 0; j < digits.length; j++) {
+                sum += digits[j] * (j + 1);
+            }
+        }
+
+        return sum;
+    }
+
+    public static void getcreateFile(File file){
+        try {
+            if (file.createNewFile()) {
+                System.out.println("File created successfully.");
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("Failed to create the file   .");
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        //arraylist
+        // arraylist
         ArrayList<String> FName = new ArrayList<>();
         ArrayList<String> Add = new ArrayList<>();
         ArrayList<String> CInfo = new ArrayList<>();
         ArrayList<String> EAdd = new ArrayList<>();
-        
-        //strings
+
+        // strings
         String clientFName;
         String clientAdd;
         String clientCInfo;
         String clientEAdd;
-        //booleans
+        // booleans
         boolean isnameValid;
         boolean isAddValid;
         boolean isContInfoValid;
         boolean isEmailAddValid;
 
-        System.out.print("Client Full Name (ex.: Paul Harris B. Bathan): ");
+        //File
+        File clientFile = new File("CLEINT text file");
+
+        System.out.print("Client Full Name (ex.: Paul Harris B. Bathan): \n");
         clientFName = sc.nextLine();
         FName.add(clientFName);
 
-        System.out.print("Client Address (ex.: Mataas na Lupa, Lipa City, Batangas): ");
+        System.out.print("Client Address (ex.: Mataas na Lupa, Lipa City, Batangas): \n");
         clientAdd = sc.nextLine();
         Add.add(clientAdd);
-    
 
-        System.out.print("Client Contact Information (ex.: 09123456789): ");
-        clientCInfo = sc.nextLine();
+        while (true) {
+            System.out.print("Client Contact Information (ex.: 09123456789): \n");
+            clientCInfo = sc.nextLine();
 
-        isContInfoValid = continfoValidation(clientCInfo);
-        if (!isContInfoValid){
-            System.out.println("Invalid Contact Information. Please try again. \n");
-        } else {
-            CInfo.add(clientCInfo);
+            isContInfoValid = continfoValidation(clientCInfo);
+            if (!isContInfoValid) {
+                System.out.println("Invalid Contact Information. Please try again. \n");
+            } else {
+                CInfo.add(clientCInfo);
+                break;
+            }
         }
 
-        System.out.print("Client Email Address (ex.: john.doe@example.com): ");
-        clientEAdd = sc.nextLine();
+        while (true) {
+            System.out.println("Client Email Address (ex.: john.doe@example.com): ");
+            clientEAdd = sc.nextLine();
 
-        isEmailAddValid = emailValidation(clientEAdd);
-        if (!isEmailAddValid){
-            System.out.println("Invalid Email Address. Please try again. \n");
-        } else {
-            EAdd.add(clientEAdd);
+            isEmailAddValid = emailValidation(clientEAdd);
+
+            if (!isEmailAddValid) {
+                System.out.println("Invalid Email Address. Please try again. \n");
+            } else {
+                EAdd.add(clientEAdd);
+                break;
+            }
+
         }
 
+        // para firstname lang lalabas sa output
+        String fullname = FName.get(0);
+        String firstname = fullname.split(" ")[0];
 
+        clearScreen();
+
+        System.out.println("Successful Registration!!! Congratulations " + firstname + "!\n");
+
+        int newClientID = ClientID(CInfo);
+
+        System.out.println("Here is your Client ID: " + newClientID);
+        System.out.print("Please do not share it to anyone");
+
+        try (BufferedWriter cleintWrite = new BufferedWriter(new FileWriter("client", true))) {
+			cleintWrite.write(firstname + newClientID);//clientID
+			cleintWrite.newLine();
+			System.out.println("Text appended successfully.");
+		} catch (IOException e) {
+			System.out.println("Failed to update the file.");
+			e.printStackTrace();
+		}
 
 
     }
 }
-
